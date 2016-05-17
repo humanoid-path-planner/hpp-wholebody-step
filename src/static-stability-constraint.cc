824da7344f1f3af9c298995e23eb9a479e3eda56
Joseph Mirabel <jmirabel@laas.fr>
2016-05-17 18:09:45 +0200

    Update to changes of hpp-constraints regarding GenericTransformation

--- src/static-stability-constraint.cc
+++ src/static-stability-constraint.cc
@@ -17,6 +17,8 @@
 // hpp-wholebody-step-planner. If not, see
 // <http://www.gnu.org/licenses/>.
 
+#include <hpp/wholebody-step/static-stability-constraint.hh>
+
 #include <boost/assign/list_of.hpp>
 #include <hpp/model/humanoid-robot.hh>
 #include <hpp/model/joint.hh>
@@ -24,14 +26,9 @@
 #include <hpp/model/center-of-mass-computation.hh>
 #include <hpp/core/config-projector.hh>
 #include <hpp/core/numerical-constraint.hh>
-#include <hpp/constraints/orientation.hh>
-#include <hpp/constraints/position.hh>
+#include <hpp/constraints/generic-transformation.hh>
 #include <hpp/constraints/relative-com.hh>
-#include <hpp/constraints/relative-orientation.hh>
-#include <hpp/constraints/relative-position.hh>
 #include <hpp/constraints/com-between-feet.hh>
-
-#include <hpp/wholebody-step/static-stability-constraint.hh>
 
 namespace hpp {
   namespace wholebodyStep {
@@ -50,6 +47,9 @@ namespace hpp {
     using hpp::core::NumericalConstraint;
     using hpp::core::ComparisonType;
     using hpp::core::ComparisonTypes;
+
+    typedef std::vector<bool> BoolVector_t;
+    using boost::assign::list_of;
 
     const std::string STABILITY_CONTEXT = "stability";
 
@@ -88,7 +88,7 @@ namespace hpp {
       // Relative orientation of the feet
       matrix3_t reference = R1T * M2.getRotation ();
       result.push_back(NumericalConstraint::create (RelativeOrientation::create
-		       (robot, joint1, joint2, reference)));
+		       ("Feet relative orientation", robot, joint1, joint2, reference)));
       result.back ()->function ().context (STABILITY_CONTEXT);
       // Relative position of the feet
       vector3_t local1; local1.setZero ();
@@ -98,20 +98,19 @@ namespace hpp {
       matrix3_t R2T (M2.getRotation ()); R2T.transpose ();
       vector3_t local2 = R2T * (global1 - M2.getTranslation ());
       result.push_back (NumericalConstraint::create (RelativePosition::create
-			(robot, joint1, joint2, local1, local2)));
+			("Feet relative position", robot, joint1, joint2, local1, local2)));
       result.back ()->function ().context (STABILITY_CONTEXT);
       // Orientation of the left foot
       reference.setIdentity ();
       result.push_back (NumericalConstraint::create (Orientation::create
-            (robot, joint1, reference, boost::assign::list_of
-			      (true)(true)(false))));
+            ("Left foot rx/ry orientation", robot, joint1, reference,
+             list_of (true)(true)(false).convert_to_container<BoolVector_t>())));
       result.back ()->function ().context (STABILITY_CONTEXT);
       // Position of the left foot
       vector3_t zero; zero.setZero ();
-      matrix3_t I3; I3.setIdentity ();
       result.push_back (NumericalConstraint::create (Position::create
-			(robot, joint1, zero, M1.getTranslation (), I3,
-			 boost::assign::list_of (false)(false)(true))));
+            ("Left foot z position", robot, joint1, zero, M1.getTranslation (),
+             list_of (false)(false)(true).convert_to_container<BoolVector_t>())));
       result.back ()->function ().context (STABILITY_CONTEXT);
       return result;
     }
@@ -130,16 +129,15 @@ namespace hpp {
       // Orientation of the left foot
       reference.setIdentity ();
       result.push_back (NumericalConstraint::create (Orientation::create
-            (robot, joint1, reference, boost::assign::list_of
-			      (false)(false)(true)),
+            ("Left foot rz orientation", robot, joint1, reference,
+             list_of (false)(false)(true).convert_to_container<BoolVector_t>()),
           core::Equality::create ()));
       result.back ()->function ().context (STABILITY_CONTEXT);
       // Position of the left foot
       vector3_t zero; zero.setZero ();
-      matrix3_t I3; I3.setIdentity ();
       result.push_back (NumericalConstraint::create (Position::create
-			(robot, joint1, zero, M1.getTranslation (), I3,
-			 boost::assign::list_of (true)(true)(false)),
+            ("Left foot xy position", robot, joint1, zero, M1.getTranslation (),
+             list_of (true)(true)(false).convert_to_container<BoolVector_t>()),
           core::Equality::create ()));
       result.back ()->function ().context (STABILITY_CONTEXT);
       return result;
@@ -150,9 +148,7 @@ namespace hpp {
      const JointPtr_t& leftAnkle, const JointPtr_t& rightAnkle,
      ConfigurationIn_t configuration)
     {
-      using boost::assign::list_of;
       vector3_t zero; zero.setZero ();
-      matrix3_t I3; I3.setIdentity ();
 
       std::vector <NumericalConstraintPtr_t> result;
       NumericalConstraintPtr_t nm;
@@ -171,7 +167,7 @@ namespace hpp {
       nm = NumericalConstraint::create (
           ComBetweenFeet::create ("ComBetweenFeet", robot, comc,
             joint1, joint2, zero, zero, robot->rootJoint (), x,
-            list_of (true)(true)(true)(true)),
+            list_of (true)(true)(true)(true).convert_to_container<BoolVector_t>()),
           ComparisonTypes::create (comps)
           );
       result.push_back (nm);
@@ -179,27 +175,27 @@ namespace hpp {
       // Orientation of the right foot
       matrix3_t reference;
       reference.setIdentity ();
-      nm = NumericalConstraint::create (
-          Orientation::create (robot, joint2, reference,
-            list_of (true)(true)(false)));
+      nm = NumericalConstraint::create (Orientation::create
+          ("Right foot rx/ry orientation", robot, joint2, reference,
+            list_of (true)(true)(false).convert_to_container<BoolVector_t>()));
       result.push_back(nm);
       result.back ()->function ().context (STABILITY_CONTEXT);
       // Orientation of the left foot
-      nm = NumericalConstraint::create (
-          Orientation::create (robot, joint1, reference,
-            list_of (true)(true)(false)));
+      nm = NumericalConstraint::create (Orientation::create
+          ("Left foot rx/ry orientation", robot, joint1, reference,
+            list_of (true)(true)(false).convert_to_container<BoolVector_t>()));
       result.push_back (nm);
       result.back ()->function ().context (STABILITY_CONTEXT);
       // Position of the right foot
-        nm = NumericalConstraint::create (
-          Position::create (robot, joint2, zero, M2.getTranslation (), I3,
-			    boost::assign::list_of (false)(false)(true)));
+      nm = NumericalConstraint::create (Position::create
+          ("Right foot z position", robot, joint2, zero, M2.getTranslation (),
+           list_of (false)(false)(true).convert_to_container<BoolVector_t>()));
       result.push_back (nm);
       result.back ()->function ().context (STABILITY_CONTEXT);
       // Position of the left foot
-      nm = NumericalConstraint::create (
-          Position::create (robot, joint1, zero, M1.getTranslation (), I3,
-			    boost::assign::list_of (false)(false)(true)));
+      nm = NumericalConstraint::create (Position::create
+          ("Left foot z position", robot, joint1, zero, M1.getTranslation (),
+           list_of (false)(false)(true).convert_to_container<BoolVector_t>()));
       result.push_back (nm);
       result.back ()->function ().context (STABILITY_CONTEXT);
       return result;
